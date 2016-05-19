c1191c0bd44c91b3be5bf4d02c9140124b3ccf6b
BuildTools <quarktheawesome@gmail.com>
2016-05-19 17:04:13 +1000

    Add basic main.c/h

---
+++ src/main.c
@@ -0,0 +1,9 @@
+/* URetro - a thing for the Wii U */
+/* https://github.com/QuarkTheAwesome/URetro */
+
+#include "main.h"
+
+int __entry_menu(int argc, char** argv) {
+	//HBL entrypoint
+	return 1;
+}
---
+++ src/main.h
@@ -0,0 +1,10 @@
+/* URetro - A thing for the Wii U */
+/* https://github.com/QuarkTheAwesome/URetro */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+//HBL entrypoint
+void __entry_main(int argc, char** argv);
+
+#endif
