a01891511256364210fb0164a670c46003e64f31
BuildTools <quarktheawesome@gmail.com>
2016-06-06 17:45:58 +1000

    Prettify APIs

---
+++ src/api/UDynLoad.h
@@ -0,0 +1,5 @@
+/* URetro - a thing for the Wii U */
+/* https://github.com/QuarkTheAwesome/URetro */
+
+//This just makes things look nicer in the codebase
+#include "UDynLoad/api/UDynLoad.h"
---
+++ src/api/libretro.h
@@ -0,0 +1,5 @@
+/* URetro - a thing for the Wii U */
+/* https://github.com/QuarkTheAwesome/URetro */
+
+//Make codebase prettier.h
+#include "libretro-common/include/libretro.h"
