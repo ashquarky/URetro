e22d258ac47a81c51d5fe2e50cce1fc4979bb159
BuildTools <quarktheawesome@gmail.com>
2016-06-07 16:04:07 +1000

    Enforce CRLF

--- src/main.c
+++ src/main.c
@@ -2,8 +2,10 @@
 /* https://github.com/QuarkTheAwesome/URetro */
 
 #include "main.h"
+#include "video.h"
 
 int __entry_menu(int argc, char** argv) {
 	//HBL entrypoint
+	initVideo();
 	return 1;
 }
